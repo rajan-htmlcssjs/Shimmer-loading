49ecfef7f59e111555a358fc42efe9d3be1027da
rajanlagah <rajanlagah@gmail.com>
2021-11-21 22:54:00 +0530

    ✨ App done

---
+++ index.js
@@ -0,0 +1,23 @@
+const imageContainer = document.querySelector('.image-container')
+const imagedescription = document.querySelector('.image-description')
+
+function setImage(){
+    imageContainer.classList.remove('loading')
+    imageContainer.innerHTML = `
+    <img src="https://images.unsplash.com/photo-1468436139062-f60a71c5c892?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="img">
+    `
+}
+
+function setText(){
+    imagedescription.classList.remove('loading')
+    imagedescription.classList.remove('loading-text')
+    imagedescription.innerHTML =
+    `
+    <span>System on Chip (SoC), Apple M1 Max chip, 10-core CPU with 8 performance cores and 2 efficiency cores, 32-core GPU, 16-core Neural Engine, Up to 400GB/s memory bandwidth</span>
+    `
+}
+
+setTimeout(()=>{
+    setImage()
+    setText()
+},5000)
